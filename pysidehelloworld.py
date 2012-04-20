0b2b53d784063b77900d2a150fa8f8a0c460ecaa
Erik Tollerud <erik.tollerud@gmail.com>
2012-04-20 03:32:04 -1000

    added pyside

---
+++ pysidehelloworld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+ 
+# Import PySide classes
+import sys
+from PySide.QtGui import QApplication, QLabel
+ 
+ 
+# Create a Qt application
+app = QApplication(sys.argv)
+# Create a Label and show it
+label = QLabel(("<font color=red size=40>Hello World</font>"))
+label.show()
+# Enter Qt application main loop
+sys.exit(app.exec_())
