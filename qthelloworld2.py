35308c9ce26c1df0b5f281a448789b99fc1ff9a4
Erik Tollerud <erik.tollerud@gmail.com>
2012-04-20 03:31:55 -1000

    added qt2

---
+++ qthelloworld2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+ 
+# Import PyQt4 classes
+import sys
+from PyQt4.QtGui import QApplication, QPushButton
+ 
+def sayHello():
+    print "Hello Keck!"
+ 
+# Create the Qt Application
+app = QApplication(sys.argv)
+# Create a button, connect it and show it
+button = QPushButton("Click me")
+button.clicked.connect(sayHello)
+button.show()
+# Run the main Qt loop
+sys.exit(app.exec_() )   
